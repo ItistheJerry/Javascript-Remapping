89213b444d54c35d776f5f3d9eb447df475886ec
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-03-28 18:57:40 +0530

    Update array.js

--- basics2/array.js
+++ basics2/array.js
@@ -1,1 +1,32 @@
-console.log("Jerry");
+// ARRAY;
+
+const myArray = [0, 4, 3, 5, 2]; // each single is an element can be anything symbol, char, numbers etc
+// array elements can't be accessed with using arbitary strings;
+
+console.log(myArray); // yes 
+
+// shallow copy;
+// of an object whose property share the same references
+
+// deep copies
+// properties do not share the same method;
+
+
+const heroes = ["Chaha choudhary", "babu bhaiya"];
+
+const myArr2 = new Array(1, 5, 6, 22, 53);
+
+console.log(heroes, myArr2);
+// console.table(heroes, myArr2);
+
+
+console.log(myArr2.length);
+
+
+
+// Array Methods;
+
+myArray.push(7);
+console.log(myArray);
+
+
