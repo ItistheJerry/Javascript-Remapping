68c72c923c0abd5d2424244af14ef126829726cc
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-03-29 17:29:13 +0530

    object.js

--- basics2/object.js
+++ basics2/object.js
@@ -1,1 +1,26 @@
+console.log("\n+++++++++++++*********** Objects1 ++++++++++++++++********\n");
+// singleton
+// when we make with constructor only singleton;
+// when declare like literals;
+// object literals = to declare objects;
 
+// Object.create;  // object made via constructor difference makes singleton
+const user1 = { // key: values,
+    name: "Jerry",
+    age: 21,
+    profession: "Inventor",
+    
+}; // object made via object literals
+
+
+
+
+
+console.log("\n+++++++++++++*********** Objects2 ++++++++++++++++********\n");
+
+
+
+
+
+
+console.log("\n+++++++++++++*********** Objects3 ++++++++++++++++********\n");
