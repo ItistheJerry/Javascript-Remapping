045e14d355f90e182d88d549f2545a129b4d7754
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-03-31 18:02:34 +0530

    function.js

--- basics2/function.js
+++ basics2/function.js
@@ -61,3 +61,48 @@ console.log(loginUserMessage("Jerry"));
 
 console.log("\n++++++++++++++++++++**************** Functions2 ++++++++++++++++++++****************\n");
 
+// used in shopping cart
+// like we dont know number of arguments arriving so we keep the parameters ready
+
+// rest operator is same as spread operator but is defined as by usecase
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200, 400, 400, 500, 2000, 4000, 5000));
+
+
+// in the scenerio of below;
+function calculateCartPrice2(val1, val2, ...num1){
+    return num1
+}
+console.log(calculateCartPrice2(200, 400, 500, 2000, 4000, 5000));
+// output is [400, 2000, 4000, 5000]
+// as the value 200 and 400 is stored in val1 and val2
+
+// objects functions
+
+const user = {
+    username: "Jerry",
+    price: "Not Possible"
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user);
+handleObject({// passing direct object
+    username: "Bam",
+    price: 399
+})
+
+// array functions
+
+const mynewArray = [200, 400, 500, 2000, 4000, 5000]
+
+// now define a function that accesses the array and passes its second value;
+function returnValue2(getArray){
+    return getArray[1]
+}
+console.log(returnValue2(mynewArray));
+
+console.log(returnValue2([300, 600, 700, 900]));
