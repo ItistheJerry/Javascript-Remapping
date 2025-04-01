3def2c049ee5644bf18b36414e556ae4d223d052
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-04-02 05:22:46 +0530

    thisandarrow.js

--- medium1/thisandarrow.js
+++ medium1/thisandarrow.js
@@ -1,6 +1,22 @@
 console.log("\n++++++++++++********* This +++++++*********\n");
 
+// tells us about current context
+
+const user = {
+    username: "Jerry",
+    price: 999,
+
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , Welcome to The future, For which all generations have been waiting for`)
+    }
+}
+
+user.welcomeMessage();
+user.username = "Samyak";
+user.welcomeMessage();
 
 
 
 console.log("\n++++++++++++********* Arrow +++++++*********\n");
+ 
