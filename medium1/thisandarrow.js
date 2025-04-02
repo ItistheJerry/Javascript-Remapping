9d9af74800da1d051f418f6ccb7944632c6e07da
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-04-02 20:42:18 +0530

    thisandarrow.js

--- medium1/thisandarrow.js
+++ medium1/thisandarrow.js
@@ -8,15 +8,72 @@ const user = {
 
 
     welcomeMessage: function() {
-        console.log(`${this.username} , Welcome to The future, For which all generations have been waiting for`)
+        console.log(`${this.username} , Welcome to The future, For which all generations have been waiting for`);
+        console.log(this);
+        
     }
 }
 
-user.welcomeMessage();
-user.username = "Samyak";
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.username = "Samyak";
+// user.welcomeMessage();
+console.log(this);
+
+// global object in browser is called window object;
+function chai(){
+    let user1 = "Jerry"
+    console.log(this.user1);
+    
+}
+chai()
+
 
 
 
 console.log("\n++++++++++++********* Arrow +++++++*********\n");
  
+
+
+// const chaii = function () {
+//     let username = "Jerry"
+//     console.log(this.username);
+    
+// }
+
+
+// Arrow Function:-
+
+const chaii = () => {
+    let username = "Jerry"
+    console.log(this.username);
+    
+}
+
+chaii();
+
+
+
+// basic syntax
+() => {}
+
+// const addtwo = (num1, num2) => {
+//     return num1+ num2;
+// }
+
+
+// implicit returns = i understood do not need to write as it is in one line;
+
+// const addtwo = (num1, num2) => num1 + num2; return had to be written without braces;
+
+const addtwo = (num1, num2) => (num1 + num2);
+
+// return an object
+
+const add = (num1, num2) => ({username: "Jerry"})
+
+
+console.log(addtwo(3, 4));
+console.log(add(5,6));
+
+
+
