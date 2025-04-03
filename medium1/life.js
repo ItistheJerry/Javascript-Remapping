dcd892fb28566d107aa9e87abf7335b3fad0bbc2
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-04-03 11:57:56 +0530

    iife.js

--- medium1/life.js
+++ medium1/life.js
@@ -1,1 +1,35 @@
-lconsole.log("\n________))_________********* Immediately invoked function expressions (IIFE) ________))_________*********\n");
+console.log("\n********* Immediately invoked function expressions (IIFE) *********\n");
+
+// when a file only has a databae connection and we require it to just start as we invoke;
+
+(function ai(){
+    console.log(`DB CONNECTED`);
+})();
+
+// to type iife
+
+// the first parenthesis is defination of function and second one is excecution, as just after the function
+
+// global scope polution the variable pollution removal leads to iife is invoked
+
+
+// ( () => {
+//     console.log(`DB CONNECTED TWO`);
+    
+// } ) () // outups error
+
+
+( function blow() { // named IIFE
+    console.log(`DB CONNECTED TWO`);
+    
+}) ();
+
+( () => {
+    console.log(`DB CONNECTED THREE`);
+    
+}) ();
+
+( (name) => { // UNNAMED IIFE WITH PARAMETER
+    console.log(`DB CONNECTED FOUR ${name}`);
+    
+}) ("JERRY");
