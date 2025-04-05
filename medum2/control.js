9e01dc02551187bdea90e0b6efe1b14f40cee5f0
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-04-06 00:16:43 +0530

    control.js

--- medum2/control.js
+++ medum2/control.js
@@ -58,3 +58,52 @@ if(score > 299){
     console.log(`User Power: ${power}`); // Local / functional scope
 }
 
+// truthy and falsy values:-
+
+// falsy :
+/*
+1. false
+2. 0
+3. -0
+4. BigInt
+5. 0n
+6. ""
+7. null
+8. undefined
+9. NaN
+*/
+
+// Truthy :
+/*
+1. "0"
+2. true
+3. String
+4. " "
+5. []
+6. {}
+7. function(){}
+*/
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0){ // here Object.keys converts keys of the object to an array and thus we can ask for length AND [ERFORM METHODS]
+    console.log("Object is Empty");
+}
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1; // we recieve 2 or more options when we use firebase or appwrite and by using this operator we can take the next best option available to us 
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+// Ternary Operator (?)
+
+const icePrice = 400;
+
+icePrice <= 80 ? console.log("Less than 80") : console.log("We have way better");
+
+// through this operator we can use the function in one line like {condition} if true (?) log this  or (:) log this:
+
