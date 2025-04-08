1d6cbed8163ce8830eb400697f4cf8f43a871fcb
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-04-08 15:24:26 +0530

    Arrayloops.js

--- biggerleague/Arrayloops.js
+++ biggerleague/Arrayloops.js
@@ -1,1 +1,28 @@
+// for of 
+console.log("\n+++++++********* FOR OF ++++++++********\n");
 
+
+["", "", ""]
+[{}, {}, {}] // objects in arrays and get the infi via .notation and square notation
+
+//
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) { // element is itereator, objects are iterable -:- object here means loop on what 
+    console.log(num);
+}
+
+
+const greetings =  "JerryJain"//["Balle", "Jai Jinendra"];
+for (const greet of greetings) {
+    console.log(`\nEach Char is: ${greet}\n`);
+    
+    
+}
+
+ // MAPS
+ console.log("\n+++++++********* MAPS ++++++++********\n");
+
+ const map = new Map() // object itself
+ map
