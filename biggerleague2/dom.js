949da0c2d0ed9c9eb7bc231fa5a4903a414452b8
JERRYisME@Samyak <jainjerry758@gmail.com>
2025-04-19 19:02:25 +0530

    dom.js

--- biggerleague2/dom.js
+++ biggerleague2/dom.js
@@ -1,1 +1,32 @@
+console.log("\n+++++++++++++************* DOM +++++++++++++*************\n");
+
+
+
+// manipulating DOM DOCUMENT OBJECT MODEL MANIPULATING CLIENT SIDE
+
+
+// innerContent VS innerText
+
+// text Content is the complete text that is to be shown;
+
+// iner text is the text that is shown;
+
+// innerHTML
+// innerhtml is the complete format and html data ion raw
+
+// nodelist and html collection arent pure array
+
+// to use map on nodelist we need to convert it inot complete array
+
+/*
+value
+HTMLCollection(4) [li.list-item, li.list-item, li.list-item, li.list-item]
+Array.from(value)
+
+*/
+
+
+// create new DOM
+
+
 
